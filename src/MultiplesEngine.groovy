c8565ed7660cad0616e1cb9eaedd971ab84b56ca
suraj neupane <surajneupane@macfi7kj46q6r.corp.local>
2025-05-20 12:14:57 +0300

    working version of application with all the tests green

--- src/MultiplesEngine.groovy
+++ src/MultiplesEngine.groovy
@@ -1,12 +1,47 @@
 class MultiplesEngine {
 
-    static void getRawDataFileForProcessing(File inputFile, String outputFileName) {
+    static void getRawDataFileForProcessing(File inputFile, File outputFile) {
         validateInputFile getInputListOfIntegers(inputFile)
-        computeMultiples(getInputListOfIntegers(inputFile), outputFileName)
+        def multiplesMap = computeMultiples(getInputListOfIntegers(inputFile))
+        createOutputResult multiplesMap, outputFile
     }
 
-    private static void computeMultiples(List<List<Integer>> inputListOfIntegers, String outputFileName) {
+    private static void createOutputResult(Map mapMultiples, File outputFile) {
+        mapMultiples.each { key, value ->
+            def expectedFormat = "$key: ${value.join(' ')}"
+            println expectedFormat
+            appendResultToOutputFile  expectedFormat, outputFile
+        }
+    }
 
+    private static void appendResultToOutputFile(String result, File outputFile) {
+        def folder = createNewFolder("outputTestFiles")
+        def targetFile = new File(folder, outputFile.getName())
+        if (!targetFile.exists()) {
+            targetFile.createNewFile()
+        }
+        targetFile.append result + "\n"
+    }
+
+    private static Map computeMultiples(List<List<Integer>> inputListOfIntegers) {
+        Map collectionOfMultiple = [:]
+        inputListOfIntegers.each { list ->
+            def numA = list[0]
+            def numB = list[1]
+            def numC = list[2]
+            def numAMultiple = (numA..numC).findAll { it ->
+                it % numA == 0
+            }
+            def numBMultiple = (numB..numC).findAll() { it ->
+                it % numB == 0
+            }
+            def key = list.last()
+            def values = (numAMultiple + numBMultiple).unique().sort().findAll {
+                it != key
+            }
+            collectionOfMultiple.putLast key, values
+        }
+        collectionOfMultiple
     }
 
     private static void validateInputFile(List<List<Integer>> inputListOfIntegers) {
@@ -25,5 +60,11 @@ class MultiplesEngine {
         inputFile.readLines().collect {
             it.split(' ').collect { it.toInteger() }
         }
+    }
+
+    static File createNewFolder(String folderName) {
+        def folder = new File(folderName)
+        folder.exists() ? folder.delete() : folder.mkdir()
+        folder
     }
 }
--- src/MultiplesEngineTest.groovy
+++ src/MultiplesEngineTest.groovy
@@ -1,56 +1,54 @@
 class MultiplesEngineTest {
     static runTest() {
-        cleanupTestData("testFilesSunshine", "testFilesEdgeCases")
+        cleanupTestData("sunshineTestFile", "failingTestFiles")
 
-        createSunshineInputFilesForTesting()
+        createSunshineInputFiles()
+        createFailingInputFiles()
 
-        createEdgeCasesInputFilesForTesting()
-
-        runTestForSunshineInputFiles("testFilesSunshine")
-        //runTestForEdgeCasesInputFiles("testFilesEdgeCases")
+        runForSunshineTests("sunshineTestFile")
+        //runForFailingTests("failingTestFiles")
     }
 
-    private static runTestForEdgeCasesInputFiles(String testFiles) {
+    private static runForFailingTests(String testFiles) {
         def folder = new File(testFiles)
         if (folder.exists()) {
             folder.eachFile { File file ->
                 def inputFileName = file.getName()
-                def outputFileName = inputFileName.replace("input", "output")
-                MultiplesEngine.getRawDataFileForProcessing file, outputFileName
+                def outputFile = new File(inputFileName.replace("input", "output"))
+                MultiplesEngine.getRawDataFileForProcessing file, outputFile
             }
         }
     }
 
-    private static runTestForSunshineInputFiles(String testFiles) {
+    private static runForSunshineTests(String testFiles) {
         def folder = new File(testFiles)
         if (folder.exists()) {
             folder.eachFile { File file ->
                 def inputFileName = file.getName()
-                def outputFileName = inputFileName.replace("input", "output")
-                MultiplesEngine.getRawDataFileForProcessing file, outputFileName
+                def outputFile = new File(inputFileName.replace("input", "output"))
+                MultiplesEngine.getRawDataFileForProcessing file, outputFile
             }
         }
     }
 
-    private static createSunshineInputFilesForTesting() {
-        def folderName = "testFilesSunshine"
-        def folder = createNewFolder(folderName)
+    private static createSunshineInputFiles() {
+        def folderName = "sunshineTestFile"
+        def folder = MultiplesEngine.createNewFolder(folderName)
         (0..9).each {
             def file = new File(folder, "input${it}.txt")
-            getSunshineTestDataToWriteInAInputFile(file)
+            getSunshineTestData(file)
         }
     }
 
-    private static createEdgeCasesInputFilesForTesting() {
-        def folderName = "testFilesEdgeCases"
-        def folder = createNewFolder(folderName)
-        (0..10).each {
-            def file = new File(folder, "input${it}.txt")
-            getEdgeCaseTestDataToWriteInAInputFile(file)
+    private static createFailingInputFiles() {
+        def folderName = "failingTestFiles"
+        def folder = MultiplesEngine.createNewFolder(folderName)
+        (0..2).each {
+            getFailingTestData(folder, it)
         }
     }
 
-    private static void getSunshineTestDataToWriteInAInputFile(File file) {
+    private static void getSunshineTestData(File file) {
         def rangeAB = [1..9]
         def rangeC = [30..70]
         def randomRows = (int) (Math.random() * 10) + 1
@@ -62,40 +60,37 @@ class MultiplesEngineTest {
         }
     }
 
-    private static void getEdgeCaseTestDataToWriteInAInputFile(File file) {
+    private static void getFailingTestData(File folder, int fileIndex) {
         def rangeAB = [1..9]
         def rangeC = [30..70]
-        def randomRows = (int) (Math.random() * 10) + 1
+        def randomRows = (int) (Math.random() * 4) + 1
         randomRows.times {
             def numberA = getRandomNumberForInputRange rangeAB
             def numberB = getRandomNumberForInputRange rangeAB
             def numberC = getRandomNumberForInputRange rangeC
             def numberEdge = getRandomNumberForInputRange rangeAB
-            def randomEdgeCase = new Random().nextInt(3)
-            switch (randomEdgeCase) {
+            switch (fileIndex) {
                 case 0:
-                    file.append("${numberA} ${numberB} ${numberEdge} ${numberC}" + "\n")
+                    new File(folder, "numberWithFourRows.txt")
+                            .append("${numberA} ${numberB} ${numberEdge} ${numberC}" + "\n")
                     break
                 case 1:
-                    file.append("${numberA} ${numberB} ${1}" + "\n")
+                    new File(folder, "targetNumberIsLower.txt")
+                            .append("${numberA} ${numberB} ${1}" + "\n")
                     break
                 case 2:
-                    file.append("${0} ${numberB} ${numberC}" + "\n")
+                    new File(folder, "numberWithZeroValue.txt")
+                            .append("${0} ${numberB} ${numberC}" + "\n")
                     break
                 default:
-                    file.append("${numberA} ${numberB} ${numberC}" + "\n")
+                    new File(folder, "numberWithZeroValue.txt")
+                            .append("${numberA} ${0} ${numberC}" + "\n")
             }
         }
     }
 
     private static getRandomNumberForInputRange(ArrayList<IntRange> range) {
         range.collect { it -> it.get((int) (Math.random() * it.size())) }.first()
-    }
-
-    private static File createNewFolder(String folderName) {
-        def folder = new File(folderName)
-        folder.exists() ? folder.delete() : folder.mkdir()
-        folder
     }
 
     private static cleanupTestData(String... folderNames) {
